cfa6a3ba766beca5fe24587d1bc007448b92f436
Ghizmilth <hidanerferrer@gmail.com>
2017-06-28 14:08:18 -0700

    first edit to security_questions.js.

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,11 @@
 console.log("security_questions.js loaded");
+
+let securityQuestions = { prompt("What is your pet name?", petName: "Amber"), prompt("Which city were you born", cityBorn: "Maracaibo"), prompt("What is your favorite color", favColor: "Purple") };
+
+for (i = 0; i < secutiryQuestions.length; i++) {
+  prompt("What is your pet name?", petName: "Amber");
+  prompt("Which city were you born", cityBorn: "Maracaibo");
+  prompt("What is your favorite color", favColor: "Purple");
+} else {
+  alert("NO answers are submitted");
+}
